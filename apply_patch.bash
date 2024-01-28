f4127c0f523da92850dea4245eb9e28ca30f3e37
干净 <54979375+du5@users.noreply.github.com>
2024-01-29 06:31:33 +0800

    upgrade 20240129 update

--- apply_patch.bash
+++ apply_patch.bash
@@ -1,17 +1,18 @@
 #!/bin/bash
-RELEASES_INFO=$(curl -L -s -H "Accept: application/vnd.github+json" https://api.github.com/repos/VeroFess/PalWorld-Server-Unoffical-Fix/releases | jq -r '.[0]')
-RELEASES_VERSION=$(echo $RELEASES_INFO | jq -r '.tag_name')
+# ram leak fix by v0.1.3.1
+# RELEASES_INFO=$(curl -L -s -H "Accept: application/vnd.github+json" https://api.github.com/repos/VeroFess/PalWorld-Server-Unoffical-Fix/releases | jq -r '.[0]')
+# RELEASES_VERSION=$(echo $RELEASES_INFO | jq -r '.tag_name')
 
-for row in $(echo "${RELEASES_INFO}" | jq -r '.assets[] | @base64'); do
-    _jq() {
-        echo ${row} | base64 --decode | jq -r ${1}
-    }
-    if [[ $(_jq '.name') == *.exe ]]; then
-        continue
-    fi
-    PATH_FILE=$(_jq '.browser_download_url')
-    curl -sqL "$PATH_FILE" -o /home/steam/Steam/steamapps/common/PalServer/Pal/Binaries/Linux/PalServer-Linux-Test
-    break
-done
+# for row in $(echo "${RELEASES_INFO}" | jq -r '.assets[] | @base64'); do
+#     _jq() {
+#         echo ${row} | base64 --decode | jq -r ${1}
+#     }
+#     if [[ $(_jq '.name') == *.exe ]]; then
+#         continue
+#     fi
+#     PATH_FILE=$(_jq '.browser_download_url')
+#     curl -sqL "$PATH_FILE" -o /home/steam/Steam/steamapps/common/PalServer/Pal/Binaries/Linux/PalServer-Linux-Test
+#     break
+# done
 
 sed -i "2i sudo chown -R steam:steam /home/steam/Steam/steamapps/common/PalServer/Pal/Saved/" /home/steam/Steam/steamapps/common/PalServer/PalServer.sh
